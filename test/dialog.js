cbdec7dd35c542c1f3d6e395bdd4a22fe2c01013
Tom 'RampantEpsilon' Ware <tomjware@gmail.com>
2018-12-28 20:22:32 -0600

    Initial Commit Revision to commit d2b8846 - Previous commit didn't add all files

---
+++ test/dialog.js
@@ -0,0 +1,13 @@
+const {ipcRenderer} = require('electron')
+
+const informationBtn = document.getElementById('information-dialog')
+
+informationBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('open-information-dialog')
+})
+
+ipcRenderer.on('information-dialog-selection', (event, index) => {
+  let message = 'You selected '
+  if (index === 0) message += 'yes.'
+  else message += 'no.'
+})
